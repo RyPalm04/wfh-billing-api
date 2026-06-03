d4b7c34f0da93aa2306ca5be1dbb51f2c6cd3be5
Ryan Palmer <ryanpalmercs@gmail.com>
2026-06-03 15:36:22 -0500

    Add tenant filter to the repository layer

--- src/main/java/com/palmer/wfhbillingapi/repository/CashAdvanceRepository.java
+++ src/main/java/com/palmer/wfhbillingapi/repository/CashAdvanceRepository.java
@@ -3,5 +3,10 @@ package com.palmer.wfhbillingapi.repository;
 import com.palmer.wfhbillingapi.model.catalog.CashAdvance;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface CashAdvanceRepository extends ListCrudRepository<CashAdvance, Integer> {
+    List<CashAdvance> findAllByTenantId(UUID tenantId);
+
 }
--- src/main/java/com/palmer/wfhbillingapi/repository/TenantSettingsRepository.java
+++ src/main/java/com/palmer/wfhbillingapi/repository/TenantSettingsRepository.java
@@ -3,5 +3,9 @@ package com.palmer.wfhbillingapi.repository;
 import com.palmer.wfhbillingapi.model.settings.TenantSettings;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface TenantSettingsRepository extends ListCrudRepository<TenantSettings, Integer> {
+    List<TenantSettings> findAllByTenantId(UUID tenantId);
 }
